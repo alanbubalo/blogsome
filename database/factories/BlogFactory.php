cfe4d10546ee40c95c6fdce99fbe3a94828a4e13
Alan Bubalo <alan.buba5@gmail.com>
2025-03-29 00:33:45 +0100

    feat: implement blog functionality with migration, model, controller, and seeder

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::query()->inRandomOrder()->first();
+
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'content' => fake()->paragraph(),
+            'user_id' => $user->id,
+        ];
+    }
+}
